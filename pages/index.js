f9843ce30237292cdde2f6f14ae9b2187a08f880
Brian <brian@brianthe.dev>
2021-08-25 07:04:07 +0700

    Move footer into a component

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-
+import Footer from '../components/footer.js'
 
 export default function Home() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
@@ -77,41 +77,7 @@ export default function Home() {
         </p>
 
       </main>
-
-      <footer className="bg-gray-700 relative pt-1">
-        <div className="container mx-auto px-6">
-          <div className="sm:flex">
-            <div className="mt-8 sm:mb-8 sm:w-full sm:px-8 flex flex-col md:flex-row justify-between">
-              <div className="mx-auto flex-1">
-                <Image src="/logo/1revrev.png" alt="0xNotes footer logo" className="" width={100} height={100}/>
-              </div>
-              <div className="flex flex-col flex-1">
-                <span className="font-bold text-white mt-4 md:mt-0 mb-2">About Us</span>
-                <span className="my-2"><a href="#" className="text-white  text-md hover:text-blue-500">About</a></span>
-                <span className="my-2"><a href="#" className="text-white  text-md hover:text-blue-500">Terms</a></span>
-                <span className="my-2"><a href="#" className="text-white  text-md hover:text-blue-500">Privacy</a></span>
-              </div>
-              <div className="flex flex-col flex-1">
-                <span className="font-bold text-white mt-4 md:mt-0 mb-2">Resources</span>
-                <span className="my-2"><a href="#" className="text-white  text-md hover:text-blue-500">FAQ</a></span>
-                <span className="my-2"><a href="#" className="text-white  text-md hover:text-blue-500">GitHub</a></span>
-                <span className="my-2"><a href="#" className="text-white  text-md hover:text-blue-500">Server Status</a></span>
-              </div>
-              <div className="flex flex-col flex-1">
-                <span className="font-bold text-white mt-4 md:mt-0 mb-2">Contact Us</span>
-                <span className="my-2"><a href="#" className="text-white  text-md hover:text-blue-500">Contact</a></span>
-                <span className="my-2"><a href="#" className="text-white  text-md hover:text-blue-500">Report Bug</a></span>
-              </div>
-            </div>
-          </div>
-          <div>
-            <p className="text-white text-center w-full pb-8">
-               © Copyright 2021 0xNotes
-            </p>
-          </div>
-        </div>
-      </footer>
-
+      <Footer></Footer>
     </div>
   )
 }
