db54b6e2b89ead11bd8723b7664eff944bec9d4b
LazyScriptKiddie <jasononggt@gmail.com>
2021-08-25 13:23:17 +0700

    add links to footer

--- components/footer.js
+++ components/footer.js
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Footer() {
     return (
@@ -11,20 +12,20 @@ export default function Footer() {
               </div>
               <div className="flex flex-col flex-1">
                 <span className="font-bold text-white mt-4 md:mt-0 mb-2">About Us</span>
-                <span className="my-2"><a href="#" className="text-white  text-md hover:text-blue-500">About</a></span>
-                <span className="my-2"><a href="#" className="text-white  text-md hover:text-blue-500">Terms</a></span>
-                <span className="my-2"><a href="#" className="text-white  text-md hover:text-blue-500">Privacy</a></span>
+                <span className="my-2"><Link href="/about"><a className="text-white  text-md hover:text-blue-500">About</a></Link></span>
+                <span className="my-2"><Link href="/terms"><a className="text-white  text-md hover:text-blue-500">Terms</a></Link></span>
+                <span className="my-2"><Link href="/privacy"><a className="text-white  text-md hover:text-blue-500">Privacy</a></Link></span>
               </div>
               <div className="flex flex-col flex-1">
                 <span className="font-bold text-white mt-4 md:mt-0 mb-2">Resources</span>
-                <span className="my-2"><a href="#" className="text-white  text-md hover:text-blue-500">FAQ</a></span>
-                <span className="my-2"><a href="#" className="text-white  text-md hover:text-blue-500">GitHub</a></span>
-                <span className="my-2"><a href="#" className="text-white  text-md hover:text-blue-500">Server Status</a></span>
+                <span className="my-2"><Link href="/faq"><a className="text-white  text-md hover:text-blue-500">FAQ</a></Link></span>
+                <span className="my-2"><a href="https://github.com/brian-the-dev/0xNotes" className="text-white  text-md hover:text-blue-500">GitHub</a></span>
+                <span className="my-2"><a href="https://stats.uptimerobot.com/zljp1HR5Dq/789017929" className="text-white  text-md hover:text-blue-500">Server Status</a></span>
               </div>
               <div className="flex flex-col flex-1">
                 <span className="font-bold text-white mt-4 md:mt-0 mb-2">Contact Us</span>
-                <span className="my-2"><a href="#" className="text-white  text-md hover:text-blue-500">Contact</a></span>
-                <span className="my-2"><a href="#" className="text-white  text-md hover:text-blue-500">Report Bug</a></span>
+                <span className="my-2"><a href="mailto:contact@0xnotes.me" className="text-white  text-md hover:text-blue-500">Contact</a></span>
+                <span className="my-2"><a href="https://github.com/brian-the-dev/0xNotes/issues" className="text-white  text-md hover:text-blue-500">Report Bug</a></span>
               </div>
             </div>
           </div>
