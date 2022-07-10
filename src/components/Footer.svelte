f8c0d1705795a107eef79898520fc5fd407ab70b
Brian <brian@brianthe.dev>
2022-07-10 18:47:25 +0700

    Update footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,35 +1,28 @@
-<footer class="relative h-0 text-white">
-	<div class="bg-gray-700 pt-1">
-		<div class="sm:flex p-8">
-			<div class="mt-8 sm:mb-8 sm:w-full sm:px-8 flex flex-col md:flex-row justify-between">
-				<div class="mx-auto flex-1">
-					<div class="w-24 flex flex-col">
-						<img src="/logo.svg" class="mx-auto w-20 h-20" alt="0xNotes Logo" />
-						<span class="mx-auto text-xl font-bold">0xNotes</span>
-					</div>
-				</div>
-				<div class="flex flex-col flex-1">
-					<span class="font-bold mt-4 md:mt-0 mb-2">About Us</span>
-					<a href="/about" class="my-2 text-md">About</a>
-					<a href="/terms" class="my-2 text-md">Terms</a>
-					<a href="/privacy" class="my-2 text-md">Privacy</a>
-				</div>
-				<div class="flex flex-col flex-1">
-					<span class="font-bold mt-4 md:mt-0 mb-2">Resources</span>
-					<a href="/faq" class="my-2 text-md">FAQ</a>
-					<a href="https://github.com/get0xNotes" class="my-2 text-md">GitHub</a>
-					<a href="https://status.0xnotes.me" class="my-2 text-md">Status</a>
-				</div>
-				<div class="flex flex-col flex-1">
-					<span class="font-bold text-white mt-4 md:mt-0 mb-2">Contact Us</span>
-					<a href="mailto:contact@0xnotes.me" class="my-2 text-md">Email</a>
-					<a href="https://github.com/get0xNotes/frontend/issues" class="my-2 text-md">Bug report</a
-					>
-				</div>
+<footer class="text-white bg-primarylight pt-1">
+	<div class="px-8 mt-8 sm:mb-8 sm:w-full md:px-16 flex flex-col md:flex-row justify-between">
+		<div class="mx-auto flex-1">
+			<div class="w-24 flex flex-col">
+				<img src="/logo.svg" class="mx-auto w-20 h-20" alt="0xNotes Logo" />
+				<span class="mx-auto text-xl font-bold">0xNotes</span>
 			</div>
 		</div>
-		<div>
-			<p class="text-white text-center w-full pb-8">© 2021-2022 0xNotes</p>
+		<div class="flex flex-col flex-1">
+			<span class="font-bold mt-4 md:mt-0 mb-2">About Us</span>
+			<a href="/about" class="my-2">About</a>
+			<a href="/terms" class="my-2">Terms</a>
+			<a href="/privacy" class="my-2">Privacy</a>
+		</div>
+		<div class="flex flex-col flex-1">
+			<span class="font-bold mt-4 md:mt-0 mb-2">Resources</span>
+			<a href="/faq" class="my-2">FAQ</a>
+			<a href="https://github.com/get0xNotes" class="my-2">GitHub</a>
+			<a href="https://status.0xnotes.me" class="my-2">Status</a>
+		</div>
+		<div class="flex flex-col flex-1">
+			<span class="font-bold text-white mt-4 md:mt-0 mb-2">Contact Us</span>
+			<a href="mailto:contact@0xnotes.me" class="my-2">Email</a>
+			<a href="https://github.com/get0xNotes/frontend/issues" class="my-2">Bug report</a>
 		</div>
 	</div>
+	<p class="text-white text-center w-full pb-8">© 2021-2022 0xNotes</p>
 </footer>
