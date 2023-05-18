5ee6ecd6c875f5aa5cfe757e58496d12455e6ac6
Kai Folf <git@fcd.im>
2023-05-18 09:38:33 +0700

    Move Socket.IO URL to .env

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,3 +1,4 @@
 import io from 'socket.io-client';
+import { PUBLIC_SOCKET_URL } from '$env/static/public';
 
-export const socket = io("wss://ws.0xnotes.me");
+export const socket = io(PUBLIC_SOCKET_URL);
